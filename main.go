4163ca4ab8f4bf336346e539f579074100e3bc63
ilangod <ilango.dhandapani@gmail.com>
2024-06-03 15:28:11 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var APP *gin.Engine
+
+func getHomePageHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"status": "hey it is up!"})
+}
+
+func main() {
+	APP = gin.Default()
+	APP.GET("/status", getHomePageHandler)
+
+	if os.Getenv("LCP") == "LOCAL" {
+		APP.Run("localhost:8081")
+	} else {
+		port := os.Getenv("PORT")
+		APP.Run(":" + port)
+	}
+}
